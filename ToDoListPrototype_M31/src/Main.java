52ff4bd696d418846277c130e06259403a13a44a
Ramon West <jgomezcu@asu.edu>
2019-04-17 23:01:56 -0700

    V1.1, 4/17/19

--- ToDoListPrototype_M31/src/Main.java
+++ ToDoListPrototype_M31/src/Main.java
@@ -1,17 +1,70 @@
-
+import java.io.IOException;
+import java.util.Scanner;
 public class Main {
 
-	public static void main(String[] args) {
-		char input;
-		while(1) {
-			// create in Aux.java file
-			//input = nextCommand();
-			switch (input) {
-				case 'C': System.out.println("create new list");
-				case 'A': System.out.println("add new entry");
-				case 'D': System.out.println("delete entry");
+	public static void main(String[] args) throws IOException {
+		ToDoList list = new ToDoList();
+		printMenu();
+		System.out.println("\nEnter a command.");
+		Scanner scan = new Scanner(System.in);
+		int input = scan.nextInt();
+		while(input != 12) {
+			if (input < 1 || input > 12) {
+				System.out.println("Enter a valid command");
 			}
+			switch (input) {
+				case 1: printMenu();
+					break;
+				case 2: //list.addTaskToList(newTask);
+					break;
+				case 3: 
+					list.deleteTask();
+					break;
+				case 4: 
+					list.changeTask();
+					break;
+				case 5: 
+					list.completeTask();
+					break;
+				case 6: 
+					list.sortByDescription();
+					list.displayToGUI();
+					break;
+				case 7: 
+					list.sortByPriority();
+					list.displayToGUI();
+					break;
+				case 8: 
+					list.sortByStatus();
+					list.displayToGUI();
+					break;
+				case 9: 
+					list.printReport();
+					break;
+				case 10: 
+					list.save();
+					break;
+				case 11: 
+					list.restore();
+					break;
+			}
+			input = scan.nextInt();
 		}
+		System.in.close();
 	}
 
+	static void printMenu() {
+		System.out.println("1: print menu");
+		System.out.println("2: add new task");
+		System.out.println("3: change task");
+		System.out.println("4: change task");
+		System.out.println("5: complete task");
+		System.out.println("6: display by description");
+		System.out.println("7: display by priority");
+		System.out.println("8: display by status");
+		System.out.println("9: print report");
+		System.out.println("10: save");
+		System.out.println("11: restore");
+		System.out.println("12: terminate program");
+	}
 }
--- ToDoListPrototype_M31/src/ToDoList.java
+++ ToDoListPrototype_M31/src/ToDoList.java
@@ -14,8 +14,68 @@ public class ToDoList {
 		ArrayList<Task> completedTasks = new ArrayList<Task>();
 		ArrayList<Task> deletedTasks = new ArrayList<Task>();
 	}	
-	
+		
 	public boolean addTaskToList(Task newTask) {
+		System.out.println("adding task to list...");
 		return false;
+	}
+	
+	void deleteTask() {
+		// delete task from list.
+		System.out.println("deleting task from list...");
+	}
+	
+	void changeTask() {
+		// edit / replace task from list.
+		System.out.println("changing task...");
+	}
+	
+	void completeTask() {
+		// change status to complete and remove from list.
+		System.out.println("setting task status to complete...");
+	}
+	
+	void displayToGUI() {
+		// display current list to GUI.
+		System.out.println("displaying To Do list...");
+	}
+	
+	void sortByDescription() {
+		// sort alphabetically by description.
+		System.out.println("sorting by description...");
+	}
+		
+	void sortByPriority() {
+		// sort by priority.
+		System.out.println("sorting by priority...");
+	}
+	
+	void sortByStatus() {
+		// sort by status.
+		System.out.println("sorting by status...");
+	}
+	
+	void reverseList() {
+		// reverse list.
+		// Useful for sorting lists by increasing or decreasing order.
+		System.out.println("reversing list...");
+	}
+	
+	void printReport() {
+		// print all content and status to a file. 
+		// requires File I/O.
+		System.out.println("printing list to report...");
+	}
+	
+	void save() {
+		// format the list and save to file. 
+		// requires File I/O.
+		System.out.println("saving list...");
+	}
+	
+	void restore() {
+		// read from file to restore previous session.
+		// requires File I/O.
+		System.out.println("restoring list...");
 	}
 }
